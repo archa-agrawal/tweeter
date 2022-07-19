6ca23a79e08ab40a2ed4df5e023b66009cb13cb6
Archana Agrawal <archana.agrawal3@outlook.com>
2022-07-19 16:45:14 -0400

    added character count

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  const initialCount = 140;
+
+  $('#tweet-text').on('input', function(event) {
+    const counter = $(this).parent().find('.counter')[0];
+    const newCount = initialCount - event.target.value.length;
+    $(counter).val(newCount);
+    if(newCount < 0){
+      $(counter).addClass('counter-minus');
+    }else{
+      $(counter).removeClass('counter-minus');
+    }
+    
+  })
+});
