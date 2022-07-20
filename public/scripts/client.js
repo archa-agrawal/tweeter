736c8bb225074325b7793605e5392018581f3875
Archana Agrawal <archana.agrawal3@outlook.com>
2022-07-20 19:37:20 -0400

    implemented get request using ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,30 +5,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
 $(document).ready(function() {
   
@@ -61,7 +37,7 @@ $(document).ready(function() {
     $($tweetName).text(tweet.user.name);
     $($tweetHandle).text(tweet.user.handle);
     $($content).text(tweet.content.text);
-    $($tweetTime).text(`${daysSince(tweet.created_at)} days ago`);
+    $($tweetTime).text(`${timeago.format(tweet.created_at)}`);
     
     return $tweetElement;
   }
@@ -73,12 +49,25 @@ $(document).ready(function() {
       $("#tweets-container").append(tweetsElement);
     });
   }
-  renderTweets(data);
 
+  $("#post-new-tweet").submit((event) => {
+    event.preventDefault();
+
+    const newTweet = $("#tweet-text").serialize();
+
+    $.ajax(`/tweets`, {
+      method: 'post',
+      data: newTweet
+    });
+  });
+
+  const loadTweets = function() {
+
+    $.ajax('http://localhost:8080/tweets', {method : 'GET'})
+    .then(response => {
+      renderTweets(response)
+    })
+  }
+  loadTweets();
 });
 
-const daysSince = (timestamp) =>{
-  let difference = new Date() - timestamp;
-  let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
-  return TotalDays;
-}
