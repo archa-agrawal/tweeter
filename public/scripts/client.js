07665bbc832bd7f48c041bfbc1dfd64d3fef4f0d
Archana Agrawal <archana.agrawal3@outlook.com>
2022-07-20 16:51:31 -0400

    added all the functionality to dynamic tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,50 +1,84 @@
+'use strict'
+
 /*
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweet =  {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1658104604092
-}
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 $(document).ready(function() {
   
-  const createTweetElement = function () {
+  const createTweetElement = function (tweet) {
     
-    const tweetElement = document.createElement('article');
-    const header = document.createElement('header');
-    const content = document.createElement('p');
-    const footer = document.createElement('footer');
-    const tweetName = document.createElement('h5');
-    const tweetAvatar = document.createElement('img');
-    const tweetHandle = document.createElement('h5');
+    const $tweetElement = $(`<article class="tweet"></article>`);
+    const $header = $(`<header class="tweet-header"></header>`);
+    const $tweetName = $(`<h5 class="tweet-name"></h5>`);
+    const $tweetAvatar = $(`<img>`);
+    const $tweetHandle = $(`<h5 class="tweet-handle"></h5>`);
+    const $content = $(`<p class="tweet-content"></p>`);
+    const $footer = 
+    $(`
+      <footer class="tweet-footer">
+        <div>
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    `);
+    const $tweetTime = $(`<p class="tweet-time"></p>`)
 
-    header.append(tweetAvatar);
-    header.append(tweetName);
-    header.append(tweetHandle)
+    $($header).append($tweetAvatar, $tweetName, $tweetHandle);
+    $($footer).prepend($tweetTime)
 
-    tweetElement.append(header);
-    tweetElement.append(content);
-    tweetElement.append(footer);
-
-    tweetAvatar.innerHTML = `href= "${tweet.user.avatars}"`;
-    tweetName.innerHTML = tweet.user.name;
-    tweetHandle.innerHTML = tweet.user.handle;
-    content.innerHTML = tweet.content.text;
-    footer.innerHTML = tweet.created_at;
+    $($tweetElement).append($header, $content, '<hr>', $footer);
+  
+    $($tweetAvatar).attr('src',`${tweet.user.avatars}`);
+    $($tweetName).text(tweet.user.name);
+    $($tweetHandle).text(tweet.user.handle);
+    $($content).text(tweet.content.text);
+    $($tweetTime).text(`${daysSince(tweet.created_at)} days ago`);
     
-    return tweetElement
+    return $tweetElement;
   }
-  const $tweet = createTweetElement(tweet);
-  console.log($tweet);
-  $('#tweets-container').append($tweet);
+  
+  const renderTweets = function(tweets) {
+    
+    tweets.forEach(tweet => {
+      const tweetsElement = createTweetElement(tweet);
+      $("#tweets-container").append(tweetsElement);
+    });
+  }
+  renderTweets(data);
 
 });
+
+const daysSince = (timestamp) =>{
+  let difference = new Date() - timestamp;
+  let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+  return TotalDays;
+}
